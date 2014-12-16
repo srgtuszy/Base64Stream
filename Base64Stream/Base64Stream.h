047c8c591ead23490ad252b1277ae0ddec08a652
Michał Tuszynski <srgtuszy@gmail.com>
2014-12-16 14:07:41 +0100

    Initial Commit

---
+++ Base64Stream/Base64Stream.h
@@ -0,0 +1,13 @@
+//
+//  Base64Stream.h
+//  Base64Stream
+//
+//  Created by Michał Tuszyński on 16/12/14.
+//  Copyright (c) 2014 iapp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Base64Stream : NSObject
+
+@end
---
+++ Base64Stream/Base64Stream.m
@@ -0,0 +1,13 @@
+//
+//  Base64Stream.m
+//  Base64Stream
+//
+//  Created by Michał Tuszyński on 16/12/14.
+//  Copyright (c) 2014 iapp. All rights reserved.
+//
+
+#import "Base64Stream.h"
+
+@implementation Base64Stream
+
+@end
